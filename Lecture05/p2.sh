d02cd77768326614567cf2358ecdeace1acba818
B246601-2023 <s2495354@ed.ac.uk>
2023-10-08 01:08:55 +0100

    solution for problem2

---
+++ Lecture05/p2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+unset IFS
+IFS=$'\t'
+count=0
+while read name email city birthday_day birthday_month birthday_year country
+do
+ if test -z ${name}
+ then
+ continue
+ else
+  if test ${country} == "country"
+  then
+  continue
+  else 
+   count=$((count+1))
+   echo -e "${count}\t${name}\t${city}\t${country}"
+ fi
+fi
+done 
